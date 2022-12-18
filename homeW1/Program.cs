fe86d8220cc9a7bc110c736a7267e00d754fb800
Alexey9983 <119534573+Alexey9983@users.noreply.github.com>
2022-12-18 20:47:00 +0300

    Задача 3

--- homeW1/Program.cs
+++ homeW1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// ЗАДАЧА №1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
+
 // Console.WriteLine("Введи число 1: ");
 // int num1 = int.Parse(Console.ReadLine()!);
 
@@ -21,32 +22,50 @@
 //         Console.WriteLine($"число {num2} больше числа {num1}");
 //     }
 
+
 // ЗАДАЧА №2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
 
-Console.WriteLine("Введи число 1: ");
-int num1 = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введи число 2: ");
-int num2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи число 1: ");
+// int num1 = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введи число 3: ");
-int num3 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи число 2: ");
+// int num2 = int.Parse(Console.ReadLine()!);
 
-int maxNum = num1;
+// Console.WriteLine("Введи число 3: ");
+// int num3 = int.Parse(Console.ReadLine()!);
 
-if (num2 > maxNum)
-    {
-       maxNum = num2; 
-    }
-    else
-    {
-         maxNum = num1;
-    }
-if (num3 > maxNum)
-    {
-       maxNum = num3; 
-    }
+// int maxNum = num1;
+
+// if (num2 > maxNum)
+//     {
+//        maxNum = num2; 
+//     }
+//     else
+//     {
+//          maxNum = num1;
+//     }
+// if (num3 > maxNum)
+//     {
+//        maxNum = num3; 
+//     }
      
-Console.WriteLine($"Максимальное из трех чисел: {maxNum}");
+// Console.WriteLine($"Максимальное из трех чисел: {maxNum}");
 
-// ЗАДАЧА №3. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+// ЗАДАЧА №3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.WriteLine("Введи число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int result = num % 2;
+
+if (result == 0)
+{
+    Console.WriteLine($"Данное число {num} - четное");
+}
+else
+{
+    Console.WriteLine($"Данное число {num} - нечетное");
+}
