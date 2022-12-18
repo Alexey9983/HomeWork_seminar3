cf7fde721577124af450c55696737f71995bca35
Alexey9983 <119534573+Alexey9983@users.noreply.github.com>
2022-12-18 20:37:56 +0300

    Задача 2

--- homeW1/Program.cs
+++ homeW1/Program.cs
@@ -1,4 +1,27 @@
-﻿// Задача №1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// ЗАДАЧА №1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введи число 1: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введи число 2: ");
+// int num2 = int.Parse(Console.ReadLine()!);
+
+// if (num1 == num2)
+// {
+//     Console.WriteLine($"числа {num1} и {num2} равны");
+// }
+   
+// else
+//     if (num1 > num2)
+//     {
+//         Console.WriteLine($"число {num1} больше числа {num2}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"число {num2} больше числа {num1}");
+//     }
+
+// ЗАДАЧА №2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
 
 Console.WriteLine("Введи число 1: ");
 int num1 = int.Parse(Console.ReadLine()!);
@@ -6,19 +29,24 @@ int num1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введи число 2: ");
 int num2 = int.Parse(Console.ReadLine()!);
 
-if (num1 == num2)
-{
-    Console.WriteLine($"числа {num1} и {num2} равны");
-}
-   
-else
-    if (num1 > num2)
+Console.WriteLine("Введи число 3: ");
+int num3 = int.Parse(Console.ReadLine()!);
+
+int maxNum = num1;
+
+if (num2 > maxNum)
     {
-        Console.WriteLine($"число {num1} больше числа {num2}");
+       maxNum = num2; 
     }
     else
     {
-        Console.WriteLine($"число {num2} больше числа {num1}");
+         maxNum = num1;
     }
+if (num3 > maxNum)
+    {
+       maxNum = num3; 
+    }
+     
+Console.WriteLine($"Максимальное из трех чисел: {maxNum}");
 
-
+// ЗАДАЧА №3. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
